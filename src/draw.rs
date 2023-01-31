f17efcbd6646c8584065d2b0cb07b4dd5822e200
Edward Taylor <edeetee@gmail.com>
2023-01-31 15:40:30 +1300

    playing

--- src/draw.rs
+++ src/draw.rs
@@ -33,7 +33,9 @@ pub fn draw(pos: Vec2, ctx: &Context) -> Rgb<palette::encoding::Srgb, u8> {
     let scale = Vec2::new(0.1, 0.03)*2.0;
     
     let animation_pos = ctx.elapsed_seconds*0.345 - (pos*Vec2::new(1.0*(1.0+(0.123*ctx.elapsed_seconds).sin()),1.0)).length() * 0.055;
-    let animated_scale = Vec2::new(1.1 + (((animation_pos.sin() * 1.44512).sin()) * 5.123).sin(), 1.0);
+    
+    let fact_sin = (((animation_pos.sin() * 1.44512).sin()) * 5.123).sin();
+    let animated_scale = Vec2::new(1.1 + 0.5 * fact_sin, 1.0);
 
     let tri_pos = pos * scale * animated_scale+pos.signum()*audio_val*1.0;
 
@@ -50,9 +52,9 @@ pub fn draw(pos: Vec2, ctx: &Context) -> Rgb<palette::encoding::Srgb, u8> {
     let brightness = 1.0 * mask;
 
     let hsv = Hsv::new(
-        (((shape_val * 5.0 + ctx.elapsed_seconds * 10.0).sin()) * 0.2 + 0.8) * 360.0,
+        (((shape_val * 10.0 + ctx.elapsed_seconds * 5.0).sin()) * 0.2 + 0.8) * 360.0,
         1.0, // ((shape_val)%1.0).powf(0.5),
-        ((shape_val*2.0 + ctx.elapsed_seconds * 0.21232) % 1.0).powi(4) * brightness,
+        ((shape_val*1.0 + ctx.elapsed_seconds * 0.0) % 1.0).powi(4) * brightness,
     );
 
     Rgb::from_color(hsv).into_format::<u8>()
