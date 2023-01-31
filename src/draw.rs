db551cbe6f570046b6a4c3a25d2bc2d0371f0185
Edward Taylor <edeetee@gmail.com>
2023-01-31 16:29:43 +1300

    ..

--- src/draw.rs
+++ src/draw.rs
@@ -37,7 +37,7 @@ pub fn draw(pos: Vec2, ctx: &Context) -> Rgb<palette::encoding::Srgb, u8> {
     // let animation_f = ctx.elapsed_seconds*1.545;
     let animation_f = ctx.elapsed_seconds*1.345 - animation_pos.length()*0.055;
 
-    let fact_sin = (((animation_f.sin() * 2.44512).sin()) * 1.123);
+    let fact_sin = ((animation_f*3.0).sin() * 2.44512).sin();
     let animated_scale = Vec2::new(1.1 + 0.3 * fact_sin, 1.0);
 
     let tri_pos = pos * scale * animated_scale+pos.signum()*audio_val*1.0;
